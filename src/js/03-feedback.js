077610571023e2f15c36bf5578762e11baf3162c
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-02-16 23:44:21 +0200

    Update 03-feedback.js
    task3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+const formData = {};
+
+const localStorage = window.localStorage;
+
+function onFormData(e) {
+  formData.email = email.value;
+  formData.message = message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function dataLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+}
+
+dataLocalStorage();
